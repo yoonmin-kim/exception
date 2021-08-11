941044dc1980b69377835ef246b0562bad44659d
yoonmin-kim <rladbsals23@gmail.com>
2021-08-11 22:22:04 +0900

    feat : @ControllerAdvice
    컨트롤러 내부에 선언한 @ExceptionHandler를 다른 컨트롤러에서도
    재사용하고 싶은 경우가 있다.
    
    @ControllerAdvice를 사용하면 글로벌한 @ExceptionHandler를 생성할 수 있고
    뿐만아니라 3가지 옵션을 통해 원하는 대상을 지정할 수 있다.
    
     - basePackages : 지정한 패키지 하위에 있는 모든 컨트롤러들이 대상
     - assignableType : 특정 클래스를 대상으로 지정할 수 있다
     - annotations : 지정한 어노테이션이 선언된 모든 컨트롤러들이 대상
     (예시) @ControllerAdvice(annotations = RestController.class)

--- src/main/java/hello/exception/api/ApiExceptionV3Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-public class ApiExceptionV2Controller {
+public class ApiExceptionV3Controller {
 
-    @GetMapping("/api2/members/{id}")
+    @GetMapping("/api3/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
